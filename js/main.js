9007d09d166338507cd94ca1c0719bc67905177e
jahidsheikh1 <jahidsheikh5152@gmail.com>
2023-09-27 20:08:49 +0600

    my first project

---
+++ js/main.js
@@ -0,0 +1,359 @@
+// console.log("hello world from console.log")
+
+
+// var nam ="shakil"
+// var nam ="tmjid"
+// var nam ="mohuya"
+// var nam ="mohima"
+// var nam ="redoan"
+
+// console.log(nam)
+
+
+
+// let nam ="mohuya"
+// let nam ="mohima"
+// let nam ="redoan"
+
+
+
+//  const x = "mohuya";
+//        x = "shakil";
+//  console.log(x);
+
+
+//  let x ="mohuya";
+//  x ="shakil";
+
+
+// var x ="mohuya"
+// var x ="shakil"
+
+// x ="shakil"
+// console.log(x);
+
+
+
+// var a = "maisha"   //this is string and also it is a primitve data type
+// console.log(a)
+
+// var b = "maisha"   //this is string and also it is a primitve data type
+// console.log(b)
+
+// var c = "maisha" //this is string and also it is a primitve data type
+// console.log(c)
+
+// var d = "maisha" //this is string and also it is a primitve data type
+// console.log(d)
+
+// var e = "maisha" //this is string and also it is a primitve data type
+// console.log(e)
+
+// var f = "maisha" //this is string and also it is a primitve data type
+// console.log(f)
+
+// var g = "maisha" //this is string and also it is a primitve data type
+// console.log(g) 
+
+// var h = "maisha"  //this is string and also it is a primitve data type
+// console.log(h)
+
+// var i = "maisha"  //this is string and also it is a primitve data type
+// console.log(i)
+
+// var j = "(maisha)" //this is string and also it is a primitve data type
+// console.log(j)
+
+
+// ************************************************
+
+
+// let num = "1"; //tis is a nuber
+// console.log(num);
+
+//  let a = "1"; //tis is anumber
+//   console.log(a);
+
+
+// let b = "2" //this is anumber
+// console.log(b)
+
+// let c = "2" //this is nuber
+// console.log(b)
+
+// let d = "2" //this is a number
+// console.log(b)
+
+// let e = "2" //this is a number
+// console.log(b)
+
+// let f = "2" //this is a number
+// console.log(b)
+
+//  let  g = "2" //this is a number
+//  console.log(g)
+
+// let h = "3" //this is a number 
+// console.log(h)
+
+// ************************************************
+
+
+// const booleen ="true" //this is boolean
+// console.log(booleen)
+
+// let t ="true" //this is boolean
+// console.log(t)
+
+
+// var boo ="true" //this is boolean
+// console.log(boo)
+
+// var bool ="true" //this is boolean
+// console.log(bool)
+
+// var bool ="true" //this is boolean
+// console.log(bool)
+
+// let a ="true" //this is boolean
+// console.log(a)
+
+// var ddd = "ssssss"
+// console.log (ddd)
+
+
+// var shakil = "shakil";
+// var shakil = "hasan";
+// var shakil = "ridoy";
+// console.log(shakil)
+
+// let hasan = "hasan";
+
+
+// ============================================
+
+
+
+// arithmetic operators
+
+
+
+// + --------> this operator called a plus or addition
+
+// - --------> this is a minus or substraction 
+
+// * --------> this is a ultiplication
+
+//  / --------> this is multiplication
+
+// % --------> this is modulus
+
+// ** -------> expential
+
+
+// var a = 20;
+// var b = 30;
+
+// var c = a + b;
+
+// console.log(c);
+
+// ans=50
+
+
+// var a = 20;
+// var b = 30;
+
+// var c = a * b;
+
+// console.log(c);
+
+// ans=600
+
+
+// var a = 20;
+// var b = 30;
+
+// var c = a ** b;
+
+// console.log(c);
+
+// ans=1.073741824e+39
+
+
+
+// var a = 20;
+// var b = 30;
+
+// var c = a - b;
+
+// console.log(c);
+
+// ans= -10
+
+
+
+// var a = 20;
+// var b = 30;
+
+// var c = a / b;
+
+// console.log(c);
+
+// ans= 0.6666666666666666
+
+
+// var a = 20;
+// var b = 30;
+
+// var c = a % b;
+
+// console.log(c);
+
+// ans= 20
+
+
+// var a = 20;
+// var b = 30;
+
+// var c = b % a;
+
+// console.log(c);
+
+// ans= 10
+
+
+// let a = 7;
+// let b = 5;
+
+// let c = a % b;
+
+// console.log(c);
+
+// ans= 0
+
+
+// let a = 10;
+
+// let c = a ** 2
+
+// console.log(c);
+// ans=100
+
+
+
+// var b = 20;
+
+// var d = b ** 2
+
+// console.log(d);
+
+// ans= 400
+
+
+// var b = 50;
+
+// var d = b ** 2
+
+// console.log(d);
+
+// ans= 2500
+
+
+
+// var b = 100;
+
+// var d = b ** 2;
+
+// console.log(d);
+
+// ans= 
+
+// var a = 20;
+// var b = a;
+// console.log(a);
+// console.log(b)
+// a = 30;
+// console.log(a);
+// console.log(b);
+
+
+
+// var a = 50;
+// var b = 50;
+// var c = a + b;
+// console.log(c)
+
+
+//  var a = 5;
+//  var b = 7;
+
+//  var c, d;
+
+//  c = a++;
+//  d = ++b;
+
+//  console.log(c);
+//   console.log(d);
+//  console.log(d)
+//  console.log(b)
+
+
+
+// 2 ** 3
+
+// 8 = 1 kore barve
+
+
+
+// var a = 8
+
+// a = 8
+
+// a ++
+
+// 1 kore barbe
+
+// 9 hoiya jabe
+
+// a++, a = a+1
+
+// var a = 5
+ // var b = 7
+// var c, d;
+
+// c = a++
+// d = ++b
+
+// console.log(c+1)
+// console.log(c)
+
+
+// console.log(d)
+// console.log(a)
+// console.log(b)
+
+
+// --------> comparison oparator
+
+
+
+// todo : non-primitive ---> Array , object , function 
+ 
+
+let myFirstArray = ["tamzid", 23444, true, undefined, null, false];
+
+// myFirstArray[0] = "gf nai";
+
+//    myFirstArray.shift();
+//    myFirstArray.unshift("redowan");
+//    myFirstArray.pop()
+
+    // myFirstArray.push("hhhhhhhhhh");
+    //   myFirstArray.splice(2, 0, "hhhhdddd");
+    //   myFirstArray.splice(0, 3,);
+    //    myFirstArray.splice(3, 1,);
+    // let newarray = myFirstArray.join("*")
+    delete myFirstArray[0];
+
+    console.log(myFirstArray);
